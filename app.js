3dad3d027f99d68e57f091fa0f1641c3bd7eef6b
Nico8893 <111314351+Nico8893@users.noreply.github.com>
2022-09-26 19:53:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ let app = express();
 
 let publicFolderPath= path.resolve(__dirname, "./public");
 app.use(express.static(publicFolderPath));
-app.listen(3000, () => {
+/*app.listen(3000, () => {
     console.log ("El servidor está corriendo")
-});
+});*/
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
 })
